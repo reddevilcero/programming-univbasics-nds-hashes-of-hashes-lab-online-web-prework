f64546c8e9e09f2ca3cc8ab3a2ddbcd4668f885f
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2020-01-16 11:39:12 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,31 +4,36 @@
 #
 # The tests will guide your construction.
 #
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-#
-# PORTION_2 = {
-#   label: "Order"
-# }
-#
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-#
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+
+
+PORTION_4 = {
+  label: "Species",
+  sub_category: nil
+}
+
+PORTION_3 = {
+  label: "Family",
+  sub_category: {
+    label: "Genus",
+    sub_category: PORTION_4
+  }
+}
+
+PORTION_2 = {
+  label: "Order",
+  sub_category: PORTION_3
+}
+
+PORTION_1 = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class",
+      sub_category: PORTION_2
+    }
+  }
+}
 
 def naming_system
   # Remember:
@@ -63,6 +68,8 @@ def naming_system
      }
    }
  }
+
+ life_in_heart = PORTION_1
 
  return life_in_heart
 end
